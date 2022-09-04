464a056de75aa79ac98b62501e8b1b97d8f16658
Flynn <Flynn@maytek.com.au>
2022-09-04 21:58:40 +1000

    menu navigation

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
-
-    public static RaceManager RaceManager { get=> Instance.raceManager; }
     RaceManager raceManager;
+    public static GameManager Instance { get; private set; }
+    public static RaceManager RaceManager { get => Instance.raceManager; }
 
     private void Awake()
     {
