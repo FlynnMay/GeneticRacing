d819067d697b71c4e0b18b83ffa350f050772df3
Flynn <Flynn@maytek.com.au>
2022-11-27 23:42:13 +1100

    Play button is not interactable if no agents are trained

--- Assets/Scripts/MapSelectionManager.cs
+++ Assets/Scripts/MapSelectionManager.cs
@@ -14,6 +14,10 @@ public class MapSelectionManager : MonoBehaviour
 
     private void Awake()
     {
+        playButton.interactable = DNAExporter.Deserialise().Count > 0;
+        TMPro.TMP_Text text = playButton.GetComponentInChildren<TMPro.TMP_Text>();
+        text.color = new Color(text.color.r, text.color.g, text.color.b, playButton.interactable ? 1.0f : 0.3f);
+
         for (int i = 0; i < mapContainer.Maps.Count; i++)
         {
             Map map = mapContainer.Maps[i];
